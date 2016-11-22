2e6788c443ba653d7ca8450c47dbc3096b40512e
gjmabo <gjmabo@gmail.com>
2016-11-21 19:44:07 -0600

    Update ADC_Proy3.ino

--- ADC_Proy3.ino
+++ ADC_Proy3.ino
@@ -11,27 +11,34 @@ volatile uint32_t *clrPin = &PORT->Group[g_APinDescription[PIN].ulPort].OUTCLR.r
 const uint32_t  PinMASK = (1ul << g_APinDescription[PIN].ulPin);
 #endif
 
-// This is an C/C++ code to insert repetitive code sections in-line pre-compilation
-// Wait for synchronization of registers between the clock domains
-// ADC
+// ADCsync se encarga de sincronizar los dominios de reloj entre el ADC y
+// el resto del microcontrolador.
+// Lee elregistro STATUS del ADC y espera hasta que se libere.
 static __inline__ void ADCsync() __attribute__((always_inline, unused));
 static void   ADCsync() {
-  while (ADC->STATUS.bit.SYNCBUSY == 1); //Just wait till the ADC is free
+  while (ADC->STATUS.bit.SYNCBUSY == 1); // Esperar hasta que el ADC se libere
 }
 
-// Variables defined
+// Variables definidas
 uint32_t Status = 0x00000000;
-  uint32_t ulPin_V = A1;      //This is the analog pin to read voltage
-uint32_t ulPin_C = A2;      //This is the analog pin to read current
-uint32_t rawDataV[1000], rawDataI[1000];           // variable to store the value read
+uint32_t ulPin_V = A1;      // Pin analogico para leer voltaje
+uint32_t ulPin_C = A2;      // Pin analogico para leer corriente
 
-int pinCrucePorCero = 11;
+// Arreglos para almacenar los datos de las conversiones.
+// Almacenan valores entre 0 y 1023, para luego ser procesados.
+// El tipo es uint32_t porque almacenan directamente el valor 
+// tomado directamente del registro de salida del ADC. 
+uint32_t rawDataV[1000], rawDataI[1000];           
+
+int pinCrucePorCero = 11;   // Pin para leer el ancho del pulso
 unsigned long pulseLenght, freq;
-int pinADCRead, range;
+int pinADCRead, range;      // range toma el valor del rango en el que se encuentra la frecuencia de entrada
 
+// Vectores para almacenar los valores ya procesados
 float Vvector[1000];
 float Ivector[1000];
 
+// Variables auxiliares para el procesamiento de datos
 float V;
 float I;
 
@@ -43,7 +50,7 @@ void setup()
   pinMode(pinCrucePorCero, INPUT);
 
   //###################################################################################
-  // ADC setup stuff
+  // ADC setup stuff (basado en las bibliotecas de Arduino)
   //###################################################################################
   ADCsync();
   ADC->INPUTCTRL.bit.GAIN = ADC_INPUTCTRL_GAIN_1X_Val;      // Gain select as 1X
@@ -68,10 +75,13 @@ void setup()
 
 void loop()
 {
-
+    // PulseIn da el valor de la duracion de un pulso en usegs
     pulseLenght = pulseIn(pinCrucePorCero, HIGH);
+    // Con ese valor se calcula la frecuencia, utilizando una función 
+    // de mejor ajuste obtenida empíricamente.
     freq = 272480*pow(pulseLenght,-0.909);
     
+    // Se escoge el rango según la frecuencia obtenida
     if ( freq > 2500 ){
         range = 1;
       }
@@ -91,29 +101,35 @@ void loop()
     else if ( (200 >= freq ) ){
         range = 5;
       }
-
+  
+  // Configurar el prescaler del ADC y el pin de lectura de Voltaje
   configADC(1, range);
 
-  for (int i=0; i<1000; i++){
+  for (int i=0; i<1000; i++){           // Para tener 1000 muestras
       rawDataV[i] = continuousADCRead();
     }
-    
+  
+  // Configurar el prescaler del ADC y el pin de lectura de Voltaje  
   configADC(2, range);
 
-  for (int i=0; i<1000; i++){
+  for (int i=0; i<1000; i++){           // Para tener 1000 muestras
       rawDataI[i] = continuousADCRead();
     }
 
   //---------------------Conversion----------------------------
 
-     for (int i=0; i<1000; i++){ //Para tener 50 muestras
+  // Se pasan los valores sin procesar a los valores reales, segun el 
+  // acondicionamiento de la señal utilizado. 
+  // Los valores procesados se almacenan en el arreglo correspondiente.
+  
+     for (int i=0; i<1000; i++){        
         V = rawDataV[i] * ((3.3)/1023.0);
         V = (V-0.9)*27.7245*2;
         Vvector[i] = V;         
       }
 
 
-     for (int i=0; i<1000; i++){ //Para tener 50 muestras
+     for (int i=0; i<1000; i++){
         I = rawDataI[i] * ((3.3)/1023.0);
         I=((I-0.8)-0.0614)/(6.1068);
         Ivector[i] = I;         
@@ -121,9 +137,11 @@ void loop()
 
 //-------------------Valores Pico-----------------------
 
-float max_auxV, max_auxI, Vpico, Ipico;
-max_auxV = 0;
-max_auxI = 0;
+  // Se obtienen los valores pico de las señales leídas.
+  
+  float max_auxV, max_auxI, Vpico, Ipico;
+  max_auxV = 0;
+  max_auxI = 0;
 
     for(int i=0; i<1000; i++){
       max_auxV = max(max_auxV, Vvector[i]);
@@ -133,11 +151,13 @@ max_auxI = 0;
       max_auxI = max(max_auxI, Ivector[i]);
       }
 
-Vpico = max_auxV;
-Ipico = max_auxI;
+  Vpico = max_auxV;
+  Ipico = max_auxI;
 
 //-------------------RMS--------------------------------
 
+  // Se obtienen los valores RMS de las señales leídas.
+  
   float auxV = 0;
   float auxI = 0;
 
@@ -155,6 +175,8 @@ Ipico = max_auxI;
   
 //------------ Pontencia Promedio------------------- 
 
+  // Calculo de la potencia promedio
+  
   float auxP = 0;
   float auxP1 = 0;
   
@@ -167,14 +189,21 @@ Ipico = max_auxI;
   float PotProm = auxP1/1000 ;
 
 //--------------------Potencia Aparente----------------------
+  
+  // Calculo de la potencia aparente
 
   float P_Aparente = ((Vrms)*(Irms));
 
 //------------------Factor de Potencia-----------------------
+  
+  // Calculo del factor de potencia
 
   float FP = ((PotProm)/(P_Aparente));
 
 //----------------Envio de los datos------------------------
+  
+  // Se envían de esta manera para que el programa Processing
+  // pueda hacer la interfaz gráfica
   
   for(int i=0; i<1000; i++){
      Serial.print(Vvector[i]);
@@ -202,31 +231,34 @@ Ipico = max_auxI;
   
 }
 
+// Funcion para implementar la lectura del registro de salida del ADC
 uint32_t continuousADCRead() {
   
   ADCsync();
 
-  while ( ADC->INTFLAG.bit.RESRDY == 0 );   // Wait till conversion done
+  while ( ADC->INTFLAG.bit.RESRDY == 0 );   // Esperar a que la conversion termine
   ADCsync();
   uint32_t valueRead = ADC->RESULT.reg;
 
-  ADC->INTFLAG.bit.RESRDY = 1;              // Data ready flag cleared
+  ADC->INTFLAG.bit.RESRDY = 1;              // Limpiar la bandera de 'Datos listos'
 
   return valueRead;
 }
 
+// Funcion para implementar la lectura en modo free-running
 void configADC(int pin, int range){
 
-  // Configura el pind desde el que se lee (voltaje o corriente)
+  // Configura el pin desde el que se lee (voltaje o corriente)
   ADCsync();
-  if (pin == 1){
+  if (pin == 1){        // Voltaje
     ADC->INPUTCTRL.bit.MUXPOS = g_APinDescription[ulPin_V].ulADCChannelNumber; 
   }
-  else if (pin == 2){
+  else if (pin == 2){   // Corriente
     ADC->INPUTCTRL.bit.MUXPOS = g_APinDescription[ulPin_C].ulADCChannelNumber;
   }
 
   // Configura la frecuencia para el modo 'free running'
+  // Se modifica el valor del prescaler.
   ADCsync();
   if (range == 1){
     //ADC->CTRLB.bit.PRESCALER = ADC_CTRLB_PPRESCALER_DIV32_Val;
@@ -262,11 +294,12 @@ void configADC(int pin, int range){
   
 }
 
+// Funcion para deshabilitar el ADC luego de que se terminan las lecturas
 void disableADC(){
 
   ADCsync();
-  ADC->CTRLA.bit.ENABLE = 0x00;             // Disable the ADC 
+  ADC->CTRLA.bit.ENABLE = 0x00;              // Deshabilita el ADC
   ADCsync();
-  ADC->SWTRIG.reg = 0x01;                    //  and flush for good measure
+  ADC->SWTRIG.reg = 0x01;                    // Se limpia como buena medida 
   
 }
